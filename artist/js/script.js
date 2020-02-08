d45107ca870eaf0d0472c8f4eca07c8bfb278962
Jungmin Harry Kim <beramode0@hotmail.com>
2020-02-08 01:05:43 -0800

    Added search function and localstorage integration

--- artist/js/script.js
+++ artist/js/script.js
@@ -1,13 +1,12 @@
 let addButton = document.getElementById('btnAddArtist');
 let aBtn = document.getElementById('btnSubmitArtist');
+let sBtn = document.getElementById('btnSearch');
 let expanded = false;
 addButton.addEventListener('click', addArtistExpand);
 aBtn.addEventListener('click', submitArtist);
+sBtn.addEventListener('click', search);
+window.onload = function() {readyArtist()};;
 
-let dBtns = document.getElementsByClassName('deletebutton');
-for (var i = 0; i < dBtns.length; i++) {
-	dBtns[i].addEventListener('click', deletenode);
-}
 
 function addArtistExpand() {
 	let addBox = document.getElementById('addbox');
@@ -59,44 +58,15 @@ function submitArtist() {
 	let aDesc = document.getElementById('adesc');
 	let aURL = document.getElementById('aurl');
 	
-	let artist = document.createElement('DIV');
-	let piccontainer = document.createElement('DIV');
-	let artistpic = document.createElement('IMG');
-	let artistinfo = document.createElement('DIV');
-	let artistname = document.createElement('H4');
-	let artistdesc = document.createElement('P');
-	let deletebutton = document.createElement('BUTTON');
-	
-	let nametext = document.createTextNode(aName.value);
-	let desctext = document.createTextNode(aDesc.value);
 	let deletetext = document.createTextNode('Delete');
 	
-	deletebutton.addEventListener('click', deletenode);
+	localStorage.setItem('aNames' + localStorage.getItem('amount'), aName.value);
+	localStorage.setItem('aDescs' + localStorage.getItem('amount'), aDesc.value);
+	localStorage.setItem('aURLs' + localStorage.getItem('amount'), aURL.value);
 	
-	artist.setAttribute('class', 'artist');
-	piccontainer.setAttribute('class', 'piccontainer');
-	artistpic.setAttribute('class', 'artistpic');
-	artistinfo.setAttribute('class', 'artistinfo');
-	artistname.setAttribute('class', 'artistname');
-	artistdesc.setAttribute('class', 'artistdesc');
-	deletebutton.setAttribute('class', 'deletebutton');
-	deletebutton.setAttribute('type', 'button');
+	addArtistItem(aName.value, aDesc.value, aURL.value, parseInt(localStorage.getItem('amount')));
 	
-	artistpic.setAttribute('src', aURL.value);
-	
-	piccontainer.appendChild(artistpic);
-	artistname.appendChild(nametext);
-	artistdesc.appendChild(desctext);
-	artistinfo.appendChild(artistname);
-	artistinfo.appendChild(artistdesc);
-	deletebutton.appendChild(deletetext);
-	artist.appendChild(piccontainer);
-	artist.appendChild(artistinfo);
-	artist.appendChild(deletebutton);
-	
-	let alist = document.getElementById('artistList');
-	alist.appendChild(artist);
-	
+	localStorage.setItem('amount', parseInt(localStorage.getItem('amount')) + 1);
 	
 	
 	// Display addbox to none
@@ -118,6 +88,86 @@ function submitArtist() {
 	expanded = false;
 }
 
+function addArtistItem(aName, aDesc, aURL, amt) {
+	let artist = document.createElement('DIV');
+	let piccontainer = document.createElement('DIV');
+	let artistpic = document.createElement('IMG');
+	let artistinfo = document.createElement('DIV');
+	let artistname = document.createElement('H4');
+	let artistdesc = document.createElement('P');
+	let deletebutton = document.createElement('BUTTON');
+	
+	let am = document.createElement('P');
+	
+	let nametext = document.createTextNode(aName);
+	let desctext = document.createTextNode(aDesc);
+	let deletetext = document.createTextNode('Delete');
+	let amtext = document.createTextNode(amt);
+	
+	deletebutton.addEventListener('click', deletenode);
+	
+	artist.setAttribute('class', 'artist');
+	piccontainer.setAttribute('class', 'piccontainer');
+	artistpic.setAttribute('class', 'artistpic');
+	artistinfo.setAttribute('class', 'artistinfo');
+	artistname.setAttribute('class', 'artistname');
+	artistdesc.setAttribute('class', 'artistdesc');
+	deletebutton.setAttribute('class', 'deletebutton');
+	deletebutton.setAttribute('type', 'button');
+	am.setAttribute('class', 'amount');
+	
+	artistpic.setAttribute('src', aURL);
+	
+	piccontainer.appendChild(artistpic);
+	artistname.appendChild(nametext);
+	artistdesc.appendChild(desctext);
+	artistinfo.appendChild(artistname);
+	artistinfo.appendChild(artistdesc);
+	deletebutton.appendChild(deletetext);
+	am.appendChild(amtext);
+	artist.appendChild(piccontainer);
+	artist.appendChild(artistinfo);
+	artist.appendChild(deletebutton);
+	artist.appendChild(am);
+	
+	let alist = document.getElementById('artistList');
+	alist.appendChild(artist);
+}
+
+function readyArtist() {
+	if (localStorage.getItem('amount') == null) {
+		localStorage.setItem('amount', '0')
+	} else {
+		for (i=0; i < parseInt(localStorage.getItem("amount")); i++) {
+			if (localStorage.getItem("aNames" + i) != null){
+				addArtistItem(localStorage.getItem("aNames" + i),localStorage.getItem("aDescs" + i),localStorage.getItem("aURLs" + i), i);
+			}
+		}
+	}
+}
+
+function search() {
+	let list = document.getElementById('artistList');
+	while (list.firstChild) {
+		list.removeChild(list.firstChild);
+	}
+	let a = document.getElementById('search');
+	b = a.value;
+	
+	for (i=0; i < parseInt(localStorage.getItem("amount")); i++) {
+		if (localStorage.getItem("aNames" + i) != null && localStorage.getItem("aNames" + i).includes(b)){
+			addArtistItem(localStorage.getItem("aNames" + i),localStorage.getItem("aDescs" + i),localStorage.getItem("aURLs" + i), i);
+		}
+	}
+}
+
 function deletenode() {
-	this.parentNode.remove();
+	let p = this.parentNode;
+	
+	let a = p.getElementsByClassName('amount')[0];
+	localStorage.removeItem('aNames' + a.textContent);
+	localStorage.removeItem('aDescs' + a.textContent);
+	localStorage.removeItem('aURLs' + a.textContent);
+	
+	p.remove();
 }
